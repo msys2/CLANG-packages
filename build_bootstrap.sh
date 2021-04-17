3333329a8ac5cf72f359bfcc2e13a9b008c89101
Jeremy Drake <github@jdrake.com>
2021-04-16 20:40:31 -0700

    retry all packages on 32-bit, up to 5 times
    Was playing whack-a-mole with packages failing to link.

--- build_bootstrap.sh
+++ build_bootstrap.sh
@@ -10,7 +10,16 @@ source "${scriptpath}/packages${stepnum}.sh"
 for p in "${pkgs[@]}"; do
   pushd mingw-w64-$p
   ${scriptpath}/fetch-validpgpkeys.sh
-  time makepkg --config /etc/makepkg_mingw.conf --noconfirm --noprogressbar --nocheck --syncdeps --rmdeps --cleanbuild --install -f || exit $?
+  if [[ $MSYSTEM == *32 ]]; then
+    i=0
+    until time makepkg --config /etc/makepkg_mingw.conf --noconfirm --noprogressbar --nocheck --syncdeps --rmdeps --cleanbuild --install -f; do
+      ret=$?
+      (( ++i == 5 )) && exit $ret
+      sleep 1
+    done
+  else
+    time makepkg --config /etc/makepkg_mingw.conf --noconfirm --noprogressbar --nocheck --syncdeps --rmdeps --cleanbuild --install -f || exit $?
+  fi
   time makepkg --config /etc/makepkg_mingw.conf --noconfirm --noprogressbar --allsource -f || exit $?
   rm -rf src pkg
   mv *.pkg.tar.* ../packages
